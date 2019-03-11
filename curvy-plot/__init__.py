a3eddb806b92849e8fb22b6bd60ca09b31d1730b
Rune Kvamme <ruk@equinor.com>
2019-03-11 13:30:13 +0100

    Testing

--- curvy-plot/__init__.py
+++ curvy-plot/__init__.py
@@ -1,6 +1,5 @@
-from . import builder
-from . import axis
+from . import plot
 
 __version__ = '0.1.0'
 __author__ = 'Joachim Holwech joachim.holwech@gmail.com'
-__all__ = ['builder', 'axis']
+__all__ = ['plot']
--- setup.py
+++ setup.py
@@ -12,6 +12,22 @@ setup(
     install_requires=[
         'numpy',
         'scipy',
+        'pandas'
+    ]
+)
+
+setup(
+    name='curvy-plot',
+    version='0.1dev',
+    packages=['curvy-plot',],
+    license='MIT',
+    long_description=open('README.md').read(),
+    keyswords='smooth curve curves interpolation swap forward prices price python forecast spline electricity gas market maximum smoothness',
+    author='Joachim Holwech',
+    author_email='jholw@equinor.com',
+    install_requires=[
+        'numpy',
+        'scipy',
         'pandas',
         'matplotlib'
     ]
